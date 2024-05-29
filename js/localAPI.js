0d73504438753c18546e5f7fc732e45a8803aab4
Michael Burg <mclburg@gmail.com>
2024-05-29 09:41:01 +0300

    new

--- js/localAPI.js
+++ js/localAPI.js
@@ -1,99 +1,22 @@
-
-let localHostUrl = "http://localhost:8001/";
-function addNewBook(book) {
-  return axios.post(localHostUrl + "books", book);
-}
-let pageNum = 1;
 url = `http://localhost:8001/books`;
-bookGrid = document.querySelector(".book-grid");
-function getPageNum(currentPageNum) {
-  pageNum = currentPageNum;
-  displayBookOnGrid();
-}
-function disableButtons(pages) {
-  if (pages == pageNum) document.querySelector(".NextPage").disabled = true;
-  else document.querySelector(".NextPage").disabled = false;
-  if (pageNum == 1) document.querySelector(".BackPage").disabled = true;
-  else document.querySelector(".BackPage").disabled = false;
-}
-function displayBookOnGrid() {
-  //   url = `http://localhost:8001/books?_page=${pageNum}&_per_page=5`;
-  bookGrid.innerHTML = "";
-  console.log(
-    `${url}?_page=${pageNum}&_per_page=5${searchFilter()}${favoritesFilter()}`
+
+function getLibraryBooks(pageNum, favoriteFilter, searchFilter) {
+  return axios.get(
+    `${url}?_page=${pageNum}&_limit=5${favoriteFilter}${searchFilter}`
   );
-  axios
-    .get(
-      `${url}?_page=${pageNum}&_per_page=5${searchFilter()}${favoritesFilter()}`
-    )
-    .then(function (response) {
-      disableButtons(response.data.pages);
-      response.data.data.forEach((book) => addBookToGrid(book));
-    })
-    .catch(function (error) {
-      console.error("Error fetching data:", error);
-    });
 }
-function filtrBySearch() {
-  displayBookOnGrid();
-}
-
-function addBookToGrid(book) {
-  const newBookDiv = document.createElement("div");
-  newBookDiv.addEventListener("mouseover", (event) => {
-    console.log("Mouse in (over)");
-  });
-  newBookDiv.className = "book";
-  newBookDiv.innerHTML = `
-    <img class="book-img" src=${book.image} alt="" />
-    <p class="book-name">${book.bookName}</p>
-    <p class="author">by ${book.authorsName[0]}</p>
-    <div class="hiddenDiv">
-    <p class="numCopies">${book.numCopies}</p>
-    <button onclick="updateNumCoppies(this,${
-      book.id
-    },1)" class="addCopy">+</button>
-    <input type="checkbox" class="favorate" 
-    ${book.favorite == "true" ? "checked" : ""}
-    onclick="changeFavorite(this,${book.id})"/>
-    <button onclick="deleteBook(${book.id})" class="delete-book">delete</button>
-    <button onclick="updateNumCoppies(this,${
-      book.id
-    },-1)" class="reduceCopy">-</button>
-    </div>
-    `;
-  bookGrid.appendChild(newBookDiv);
-}
-function updateNumCoppies(_this, id, change) {
-  event.preventDefault();
-
-  const elem = _this.parentNode.parentNode.querySelector(".numCopies");
-  const currentNumCopies = parseInt(elem.innerText);
-  if (currentNumCopies == 0 && change == "-1") return;
-  elem.innerText = currentNumCopies + parseInt(change);
-
+function updateNumCoppiesToJson(id, currentNumCopies, change) {
   axios.patch(`${url}/${id}`, {
     numCopies: currentNumCopies + parseInt(change),
   });
 }
-
-function deleteBook(id) {
-  axios.delete(`${url}/${id}`);
-}
-displayBookOnGrid();
-function changeFavorite(_this, id) {
-  if (_this.checked) axios.patch(`${url}/${id}`, { favorite: "true" });
+function changeBookFavorite(id, bool) {
+  if (bool) axios.patch(`${url}/${id}`, { favorite: "true" });
   else axios.patch(`${url}/${id}`, { favorite: "false" });
 }
-function showFavorites() {
-  displayBookOnGrid();
+function deleteBookFromLirary(id) {
+  axios.delete(`${url}/${id}`);
 }
-function searchFilter() {
-  search = `&bookName=${document.querySelector(".search-bar").value}`;
-  return search.trim() || "";
-}
-function favoritesFilter() {
-  if (document.querySelector(".favoritesSelector").checked)
-    return "&favorite=true";
-  return "";
+function addNewBook(book) {
+  return axios.post(localHostUrl + "books", book);
 }
